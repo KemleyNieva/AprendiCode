c188abb959ff438e26fd5dbc76d2add29a27877a
Kemley <kemley@ufl.edu>
2019-01-29 18:20:44 -0500

    Routing added
    Added some routing stuff

---
+++ aprendicode/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Login extends React.Component{
+    render(){
+        return(
+          <div>
+          <p>This is the login page</p>
+          </div>
+        );
+    }
+}
