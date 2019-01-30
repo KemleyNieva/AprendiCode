c6e62ea2ca8bee3d76ed7a9991e232b9f3a28c72
Kemley <kemley@ufl.edu>
2019-01-29 19:02:29 -0500

    Added background

--- aprendicode/src/components/About.js
+++ aprendicode/src/components/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './About.css';
 
 export class About extends React.Component{
     render(){
         return(
-          <div>
-          <p>This is about me</p>
-          </div>
+            <div class="stripes">
+            <p class="outline">
+            <h1 class="title">About us</h1>
+            </p>
+            </div>
         );
     }
 }
