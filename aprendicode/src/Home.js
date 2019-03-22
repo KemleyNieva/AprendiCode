bc0cf54b7722007d14e37331d4661c7888fac3a0
Sabrina Vega <sabrina.alicia24@gmail.com>
2019-03-22 11:04:43 -0400

    homeAuth and homeNonAuth elements

--- aprendicode/src/Home.js
+++ aprendicode/src/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from './SignOut';
+import {AuthUserContext} from './constants/Session';
 
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <AuthUserContext.Consumer>
+      {authUser => 
+        authUser? <HomeAuth/> : <HomeNonAuth/>
+      }
+      </AuthUserContext.Consumer>
+    
+  </div>
+);
+
+const HomeAuth = () => (
+  <div>
+    <h1>Home Signed In</h1>
     <li><SignOutButton /></li>
   </div>
 );
 
+const HomeNonAuth = () =>(
+  <div>
+    <h1>Home Not Signed In</h1>
+</div>
+
+);
 export default Home;
