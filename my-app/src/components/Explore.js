4f3d4c23d2e4df9f774e3f05b75cb472874b3d3d
Stivixd123 <estebangonzalez20@live.com>
2019-03-28 14:21:45 -0400

    more edits and trying to fix the error for kem

--- my-app/src/components/Explore.js
+++ my-app/src/components/Explore.js
@@ -2,13 +2,57 @@ import React from "react";
 import './Explore.scss';
 import Table from 'react-bootstrap/Table';
 
+import codeMonkey from '../assets/codeMonkey.png';
+import scratch from '../assets/scratch.png';
+import tynker from '../assets/tynker.png';
+
 
 export class Explore extends React.Component {
     render() {
         return (
             <div className="about">
                 <div className="about-section last-section">
-                    <h2 className="about-section-tittle">Who We Are</h2>
+
+                    <div className="explore-cards">
+                        <div className="explore-card explore-card1">
+                            <div className="explore-card-box">
+                                <div className="explore-card-img">
+                                    <img className="explore-card-imgg" src={codeMonkey} alt="logo"></img>
+                                </div>
+                                <div className="explore-card-info">
+                                    <h2>Code Monkey</h2>
+                                    <a href="#">https://www.playcodemonkey.com</a>
+                                    <p>Online game in ENGLISH ONLY for children to learn to code in a easy manner.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="explore-card explore-card2">
+                            <div className="explore-card-box">
+                                <div className="explore-card-img">
+                                    <img className="explore-card-imgg" style={{marginTop: 8 +'px'}} src={scratch} alt="logo"></img>
+                                </div>
+                                <div className="explore-card-info">
+                                    <h2>Scratch</h2>
+                                    <a href="#">https://scratch.mit.edu</a>
+                                    <p>A  Downloadable video game avaliable in spanish for children.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="explore-card explore-card3">
+                            <div className="explore-card-box">
+                                <div className="explore-card-img">
+                                    <img className="explore-card-imgg" style={{marginTop: 41 +'px'}} src={tynker} alt="logo"></img>
+                                </div>
+                                <div className="explore-card-info">
+                                    <h2>Tynker</h2>
+                                    <a href="#">https://www.tynker.com</a>
+                                    <p>A interactive video game for children with lesson plans.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <h2 className="about-section-tittle">Who We Are</h2>
                     <div className="about-section-flex">
                         <div className="card">
                             <Table striped bordered hover className="table">
@@ -43,7 +87,7 @@ export class Explore extends React.Component {
                                 </tbody>
                             </Table>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
--- my-app/src/components/Learn.js
+++ my-app/src/components/Learn.js
@@ -8,39 +8,42 @@ export class Learn extends React.Component {
                 <div className="learn-section last-section">
                     <div className="learn-section-flex">
                         <div className="learn-card1">
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text" style={{marginTop: 205 +"px"}}>En esta lección, vas a ser un nuevo jugador de fútbol en el Equipo Técnico. Pasarás por una serie de lecciones para enseñarte los fundamentos y ¡conocer al equipo!</p>
                             <div className="learn-card-bottom">
-                                <h2 className="learn-card-bottom-h2">For Loops</h2>
+                                <h2 className="learn-card-bottom-h2">Fundamentos</h2>
                             </div>
                         </div>
                         <div className="learn-card1">
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text" style={{marginTop: 160 +"px"}}> Los estrings de tipo variable, como vimos antes, contienen principalmente palabras. Gracias al práctico lenguaje Python y al codificador anterior, los estrings vienen con una serie de comandos o acciones que pueden ayudar a facilitar la comunicación.</p>
                             <div className="learn-card-bottom">
-                                <h2 className="learn-card-bottom-h2">For Loops</h2>
+                                <h2 className="learn-card-bottom-h2">Strings</h2>
                             </div>
                         </div>
                         <div className="learn-card1">
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text" style={{marginTop: 180 +"px"}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             <div className="learn-card-bottom">
-                                <h2 className="learn-card-bottom-h2">For Loops</h2>
+                                <h2 className="learn-card-bottom-h2">Condicionales</h2>
                             </div>
                         </div>
                     </div>
                     <div className="learn-section-flex">
                         <div className="learn-card">
                             <div className="learn-card-top">
+                                <h2 className="learn-card-top-h2">Arrays</h2>
                             </div>
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                         <div className="learn-card">
                             <div className="learn-card-top">
+                                <h2 className="learn-card-top-h2">Vueltas</h2>
                             </div>
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                         <div className="learn-card">
                             <div className="learn-card-top">
+                                <h2 className="learn-card-top-h2">Funciones</h2>
                             </div>
-                            <p className="learn-card-text"> More info on loops</p>
+                            <p className="learn-card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                     </div>
                 </div>
