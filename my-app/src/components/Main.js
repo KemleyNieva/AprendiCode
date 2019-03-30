2183d05b105773c936594aeecf063aa58ecd7503
Stivixd123 <estebangonzalez20@live.com>
2019-03-30 12:16:46 -0400

    quick updates

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -5,12 +5,13 @@ import {About} from './About'
 import {Explore} from './Explore'
 import {Learn} from './Learn'
 import {Start} from './Start'
+import {Lesson} from './Lesson'
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
-      <Route exact path='/about' component={About}/>
+      <Route exact path='/about' component={Lesson}/>
       <Route exact path='/explore' component={Explore}/>
       <Route exact path='/learn' component={Learn}/>
       <Route exact path='/signin' component={Start}/>
