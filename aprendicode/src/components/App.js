39ba9471715a16d0f6266192c3358fd64cf6d545
Stivixd123 <estebangonzalez20@live.com>
2019-04-10 17:21:52 -0400

    Added Lessons and functionality to those pages. Missing content

--- aprendicode/src/components/App.js
+++ aprendicode/src/components/App.js
@@ -16,7 +16,20 @@ import {About} from './About'
 import {Explore} from './Explore'
 import {Learn} from './Learn'
 import {Start} from './Start'
-import {Lesson} from './Lesson'
+import {Lesson11} from './lessons/Lesson11'
+import {Lesson12} from './lessons/Lesson12'
+import {Lesson13} from './lessons/Lesson13'
+import {Lesson14} from './lessons/Lesson14'
+import {Lesson15} from './lessons/Lesson15'
+import {Lesson16} from './lessons/Lesson16'
+import {Lesson17} from './lessons/Lesson17'
+import {Lesson21} from './lessons/Lesson21'
+import {Lesson22} from './lessons/Lesson22'
+import {Lesson23} from './lessons/Lesson23'
+import {Lesson24} from './lessons/Lesson24'
+import {Lesson25} from './lessons/Lesson25'
+import {Lesson26} from './lessons/Lesson26'
+import {Lesson27} from './lessons/Lesson27'
 
 const App = () => (
   <Router>
@@ -28,7 +41,20 @@ const App = () => (
     <Route path={ROUTES.EXPLORE} component={Explore} />
     <Route path={ROUTES.LEARN} component={Learn} />
     <Route path={ROUTES.START} component={Start} />
-    <Route path={ROUTES.LESSON} component={Lesson} />
+    <Route path={ROUTES.LESSON11} component={Lesson11} />
+    <Route path={ROUTES.LESSON12} component={Lesson12} />
+    <Route path={ROUTES.LESSON13} component={Lesson13} />
+    <Route path={ROUTES.LESSON14} component={Lesson14} />
+    <Route path={ROUTES.LESSON15} component={Lesson15} />
+    <Route path={ROUTES.LESSON16} component={Lesson16} />
+    <Route path={ROUTES.LESSON17} component={Lesson17} />
+    <Route path={ROUTES.LESSON21} component={Lesson21} />
+    <Route path={ROUTES.LESSON22} component={Lesson22} />
+    <Route path={ROUTES.LESSON23} component={Lesson23} />
+    <Route path={ROUTES.LESSON24} component={Lesson24} />
+    <Route path={ROUTES.LESSON25} component={Lesson25} />
+    <Route path={ROUTES.LESSON26} component={Lesson26} />
+    <Route path={ROUTES.LESSON27} component={Lesson27} />
     <Footer />
   </Router>
 );
